bbd8ffdee378f3625a05406fa288bc68df6c44dc
Lazar <riliano@live.com>
2017-03-26 22:15:41 +0300

    Added bullet visualizer, replaced the current input method with a simpler one

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include<vector>
 #include<thread>
 #include<sstream>
+#include<cmath>
 
 SDL_Window *window;
 SDL_Event e;
@@ -14,7 +15,7 @@ struct path_t
 {
 	int speed;
 	int time;
-	double angle;
+	int angle;
 };
 
 struct bullet_t
@@ -23,7 +24,15 @@ struct bullet_t
 	int startY;
 	int x;
 	int y;
+	int pathIter = 0;
+	int lastPathStartTime = 0;
 	std::vector< path_t > path;
+
+	void Set( int myX, int myY )
+	{
+		x = myX;
+		y = myY;
+	}
 
 	void Reset()
 	{
@@ -35,7 +44,7 @@ struct bullet_t
 	{
 		startX = myX;
 		startY = myY;
-		Reset();
+		Set( myX, myY );
 	}
 };
 
@@ -56,18 +65,53 @@ void Input( char *type, int *info, int *infoSize, bool *done )
 	*done = true;
 }
 
+void DrawCircle( SDL_Renderer *renderer, int x, int y, int r )
+{
+	for( int i=0;i<360;i++ )
+	{
+		SDL_RenderDrawLine( renderer, x+std::sin(i)*r, y+std::cos(i)*r, x-std::sin(i)*r, y-std::cos(i)*r );
+	}
+//	SDL_RenderDrawLine( renderer, x, y+r, x, y-r );
+//	SDL_RenderDrawLine( renderer, x+r, y, x-r, y );
+}
+
 std::vector< bullet_t > bullets;
+
+const int BULLET_RADIUS = 5;
 
 int main()
 {
-	window = SDL_CreateWindow( "Bullet", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, scrWdth, scrHght, SDL_WINDOW_SHOWN );
-	SDL_Renderer *renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
 
 	char type;
 	int parameters[50];
 	int numParameters = 0;
 	bool doneInput = false;
-	std::thread input( Input, &type, parameters, &numParameters, &doneInput );
+
+	while( std::cin )
+	{
+		std::cin>>type;
+		if( type == 'b' )
+		{
+			int x, y;
+			std::cin>>x>>y;
+			bullets.push_back( {x, y} );
+		}
+		if( type == 'p' )
+		{
+			int own;
+			std::cin>>own;
+			int time, angle, speed;
+			std::cin>>speed>>angle>>time;
+			bullets[own].path.push_back( {speed, time, angle} );
+		}
+	}
+//	std::thread input( Input, &type, parameters, &numParameters, &doneInput );
+
+	window = SDL_CreateWindow( "Bullet", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, scrWdth, scrHght, SDL_WINDOW_SHOWN );
+	SDL_Renderer *renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+
+	long long time = 0;
+	long long T = SDL_GetTicks();
 
 	while( true )
 	{
@@ -80,9 +124,38 @@ int main()
 				return 0;
 			}
 		}
+		if( SDL_GetTicks() - T >= 10 )
+		{
+			for( int i=0;i<bullets.size()-1;i++ )
+			{
+				if( !bullets[i].path.empty() )
+				{
+					double cos = std::cos( (double)bullets[i].path[ bullets[i].pathIter ].angle*M_PI/180  );
+					double sin = std::sin( (double)bullets[i].path[ bullets[i].pathIter ].angle*M_PI/180  );
+
+					int newX = bullets[i].path[ bullets[i].pathIter ].speed;
+					int newY = 0;
+
+					bullets[i].Set( bullets[i].x+(newX*cos - newY*sin), bullets[i].y+(newY*cos + newX*sin) );
+					DrawCircle( renderer, bullets[i].x, bullets[i].y, BULLET_RADIUS );
+					bullets[i].lastPathStartTime++;
+					if( bullets[i].lastPathStartTime >= bullets[i].path[ bullets[i].pathIter ].time )
+					{
+						bullets[i].lastPathStartTime = 0;
+						bullets[i].pathIter++;
+						if( bullets[i].pathIter >= bullets[i].path.size() )
+							bullets[i].pathIter = 0;
+						std::cout<<i<<" "<<bullets[i].y<<" "<<sin<<" "<<cos<<" "<<bullets[i].pathIter<<std::endl;
+					}
+				}
+			}
+			time++;
+			T = SDL_GetTicks();
+		}
+
 		if( doneInput )
 		{
-			input.join();
+//			input.join();
 
 			if( type == 'q' )
 			{
@@ -125,7 +198,7 @@ int main()
 			}
 
 			doneInput = false;
-			input = std::thread( Input, &type, parameters, &numParameters, &doneInput );
+//			input = std::thread( Input, &type, parameters, &numParameters, &doneInput );
 		}
 		SDL_RenderPresent( renderer );
 		SDL_RenderClear( renderer );
